9c31948023769988b859ffa4a808d355f0123898
Mustafa-Hassan2001 <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2022-05-18 02:10:57 +0500

    Add files via upload

---
+++ web development/app.js
@@ -0,0 +1,18 @@
+var arr = [1,2,3,4,5,6,7];
+// console.log(arr);
+
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+// console.log("================");
+
+// do{
+//     console.log(arr[j]);
+//     j++
+// }while(j<arr.length)
